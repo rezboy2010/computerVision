0318412d815851eb92c4a959ba33c02c84887921
Alexander Rezyapkin <rezyapkin.alex@yandex.ru>
2025-12-05 15:01:56 +0300

    Beginning2

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import mediapipe as mp
+from config import *
+import numpy as np
 import cv2
 
 
-with mp.solutions.pose.Pose() as pose_detector:
+with (mp.solutions.pose.Pose() as pose_detector, mp.solutions.hands.Hands() as hand_detector):
     cap = cv2.VideoCapture(0)
 
     while cap.isOpened():
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
         ret, frame = cap.read()
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(np.fliplr(frame), cv2.COLOR_BGR2RGB)
         results = pose_detector.process(img)
 
         if results.pose_landmarks:
             landmarks = results.pose_landmarks.landmark
 
         res_image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        cv2.imshow("Arrow of Fate", res_image)
+        cv2.imshow(WIN_NAME, res_image)
+
+        if cv2.getWindowProperty(WIN_NAME, cv2.WND_PROP_VISIBLE) < 1:
+            break
+
+    cap.release()
+
+cv2.destroyAllWindows()
